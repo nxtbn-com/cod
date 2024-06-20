9a8a9ff6806bd0af4c800a7d32ca85a2cde69f9e
pydude <45510606+MySecondLanguage@users.noreply.github.com>
2024-06-20 17:10:10 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,3 +1,15 @@
+metadata = {
+    "plugin_name": "code",
+    "plugin_type": "PAYMENT_PROCESSOR",
+    "plugin_uri": "https://github.com/nxtbn-com/cod",
+    "version": "1.0.1",
+    "author": "bytenyx limited",
+    "author_uri": "http://bytenyx.com",
+    "description": "Plugin to handle payment via cash on delivery.",
+    "license": "BSD-3-Clause",
+    "nxtbn_version_compatibility": ">=1.0.0"
+}
+
 from . cash_on_delivery import CashOnDelivery
 
 gateway = CashOnDelivery
