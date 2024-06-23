a4f31804635eab70ad5dd03e50ee9c252eacea69
pydude <45510606+MySecondLanguage@users.noreply.github.com>
2024-06-23 23:07:09 +0600

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -7,7 +7,13 @@ metadata = {
     "author_uri": "http://bytenyx.com",
     "description": "Plugin to handle payment via cash on delivery.",
     "license": "BSD-3-Clause",
-    "nxtbn_version_compatibility": ">=1.0.0"
+    "nxtbn_version_compatibility": ">=1.0.0",
+    "has_urls": True,
+    "environment_variables": {
+        'STRIPE_SECRET_KEY': 'secret',
+        'STRIPE_PUBLIC_KEY': 'str',
+        'USE_STRIPE_INVOICE': 'bool'
+    }
 }
 
 from . cash_on_delivery import CashOnDelivery
